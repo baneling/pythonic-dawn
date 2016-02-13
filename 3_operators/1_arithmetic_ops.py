122009bf2ca43f515cfc45a0171978ff36d1f07b
Sonder <HDApperloo@gmail.com>
2016-02-14 00:36:23 +0100

    Update 1_arithmetica

--- 3_operators/1_arithmetic_ops.py
+++ 3_operators/1_arithmetic_ops.py
@@ -5,24 +5,48 @@
 # To open a shell from terminal or command: $ python
 # There are other ways to acces a python shell, like IDLE, or a web shell.
 
-#Additor +
+#Accumulator +
 10 + 3
+#> 13
 
 #Subtracter -
 10 - 3
+#> 7
 
 # Multiplicator *
 10 * 3
+#> 30
 
 # Divider /
 #How much times can you grab 3 out of a basket of 10.
-10 / 3 
+10 / 3
+#> 3
 
 # Modulus %
 #What's left after continuously grabbing 3 out of a basket of 10 until there are no groups of 3 to grab left.
-10 % 3 
+10 % 3
+#> 1
 
 # Exponent **
 # 10 * 10 * 10
 10 ** 3
+#> 1000
 
+
+
+
+
+
+
+
+
+
+
+
+# Dont worry about this stuff yet.
+print ("The + operator (accumulator): 10 + 3 = %d" % (10+3))
+print ("The - operator (substractor): 10 - 3 = %d" % (10-3))
+print ("The * operator (multiplicator): 10 * 3 = %d" % (10*3))
+print ("The / operator (divider): 10 / 3 = %d" % (10/3))
+print ("The % operator (modulus): 10 % 3 = %d" % (10%3))
+print ("The ** operator (exponent): 10 ** 3 = %d" % (10**3))
