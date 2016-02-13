45b0d5b2b25104c72fc056bec702aa480ae27d12
Sonder <HDApperloo@gmail.com>
2016-02-14 00:28:42 +0100

    No shennanigans

--- 1_hello/3_inputuser.py
+++ 1_hello/3_inputuser.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
 
-user = input("Type in your name:")
-print("Hello" + user)
-
-## Slightly improved 
-#user = str(input("Type in your name: "))
-#print ("Hello " + user)
+user = input("Type in your name: ")
+print("Hello " + user)
