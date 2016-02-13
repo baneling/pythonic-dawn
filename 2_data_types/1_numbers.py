f253777d2783985ffd25e5e871d91334a96c8cdd
baneling <hdapperloo@gmail.com>
2016-02-14 00:45:40 +0100

    building, revising

---
+++ 2_data_types/1_numbers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Standard Data Types:
+### The Number (13)
+
+## Integers
+# Decimal Number
+13
+# Hexadecimal Number
+0x0d
+# Octal Number
+0015
+
+## Floating Point Numbers
+# Float
+13.0
+# Scientific Number
+1.3E1 
+
+### Complex (STAHP)
+(13+0j)
+-4j**2-3
