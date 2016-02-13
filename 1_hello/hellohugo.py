5a27dbf6973e0423ae0f96100df43aba0664ef56
baneling <hdapperloo@gmail.com>
2016-02-13 18:33:58 +0100

    Small rectifications in hello/

--- 1_hello/hellohugo.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-user = "Hugo"
-print (user)
--- 1_hello/hellouser.py
+++ 1_hello/hellouser.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-user = input("Type in your name:")
-print("Hello" + user)
+user = "Young Pythoneer"
+print("Hello " + user)
 
-## Slightly improved 
-#user = str(input("Type in your name: "))
-#print ("Hello " + user)
